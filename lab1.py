1d837b8f0dd562b07a38f6e43a3ea05acb1f603c
TpaBHuK <ekosminin@gmail.com>
2024-10-10 18:40:05 +0300

    Update lab1.py
    Code is writed

--- lab1.py
+++ lab1.py
@@ -1,1 +1,4 @@
+import numpy as np
 
+x,y,z=float(input('Введите переменные')),float(input('Введите переменные')),float(input('Введите переменные'))
+print(np.round((np.sqrt(10*((x)**1/3)+x**(y+2))*((np.arcsin(np.radians(z)))**2-abs(x))),2))
